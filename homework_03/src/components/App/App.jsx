6d129f95cc9dbe1e7ba0f954fc7f4e21706d4fd9
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-03-03 21:52:50 +0100

    homework_03

---
+++ homework_03/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import Rating from '../Rating/Rating';
+import List from '../List/List';
+
+import '../../styles/style.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <Rating initialValue={0} />
+
+      <List  />
+    </>
+  )
+}
+
+export default App
