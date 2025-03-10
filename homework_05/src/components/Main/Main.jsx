106c5135c05cb97e8e6c4105ac46524770bbf5bb
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-03-10 20:39:10 +0100

    homework 05

---
+++ homework_05/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import StartingGreeting from '../StartingGreeting/StartingGreeting';
+
+import styles from './Main.module.css';
+
+const Main = ()=> {
+    return (
+        <main>
+            <StartingGreeting />
+        </main>
+    )
+};
+
+export default Main;
