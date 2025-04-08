88c163d63a4bf90b3f6cf3522a500731ecfbd38c
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-04-08 17:45:47 +0200

    homework_13

---
+++ homework_13/src/components/App/App.jsx
@@ -0,0 +1,16 @@
+
+import UsersView from '../UsersView/UsersView';
+
+import '../../styles/style.css'
+
+function App() {
+
+
+  return (
+    <>
+      <UsersView />
+    </>
+  )
+}
+
+export default App
